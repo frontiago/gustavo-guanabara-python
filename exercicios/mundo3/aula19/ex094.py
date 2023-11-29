115a4c3681e186b56c1905c606467f4b1899e524
Thiago Silva <thiagowfer@gmail.com>
2023-11-29 07:30:18 -0300

    Finished exercise 094

--- exercicios/mundo3/aula19/ex094.py
+++ exercicios/mundo3/aula19/ex094.py
@@ -8,3 +8,50 @@
 # C) Uma lista com todas as mulheres.
 # D) Uma lista com todas as pessoas com idade acima da média
 
+galera = list()
+pessoa = dict()
+soma = media = 0
+
+while True:
+    pessoa.clear()
+    pessoa['nome'] = str(input('Nome: '))
+
+    while True:
+        pessoa['sexo'] = str(input('Sexo [M/F] ')).upper()[0]
+        if pessoa['sexo'] in 'MF':
+            break
+        print('ERROR, por favor, digite apenas M ou F')
+
+    pessoa['idade'] = int(input('Idade: '))
+    soma += pessoa['idade']
+    galera.append(pessoa.copy())
+
+    while True:
+        resposta = str(input('Quer continuar? [S/N] ')).upper()[0]
+        if resposta in 'SN':
+            break
+        print('ERRO, responda apenas S ou N')
+    if resposta == 'N':
+        break
+
+print('-=' * 30)
+print(f'A) Ao todo temos {len(galera)} pessoas cadastradas')
+
+media = soma / len(galera)
+print(f'B) A média de idade é de {media:.0f} anos')
+
+print('C) As mulheres cadastradas foram ', end='')
+for p in galera:
+    if p['sexo'] in 'Ff':
+        print(f'{p["nome"]}, ', end='')
+print()
+
+print('D) A lista de pessoas acima da média: ', end='')
+for p in galera:
+    if p['idade'] >= media:
+        print('   ')
+        for k, v in p.items():
+            print(f'{k} = {v}; ', end='')
+        print()
+
+print('<<< ENCERRADO >>>')
