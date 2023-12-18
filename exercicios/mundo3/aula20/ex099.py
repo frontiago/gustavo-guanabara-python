8378e320f08844128db7d7a638e6f17863beebbb
Thiago Silva <thiagowfer@gmail.com>
2023-12-18 07:48:31 -0300

    Finished exercise 099

--- exercicios/mundo3/aula20/ex099.py
+++ exercicios/mundo3/aula20/ex099.py
@@ -3,4 +3,29 @@
 
 # Faça um programa que tenha uma função chamada maior(), que receba vários parâmetros com valores inteiros.
 # Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
+from time import sleep
 
+
+def bigger(*num):
+    contador = maior = 0
+    print('')
+    print('Analisando valores...')
+    for valor in num:
+        print(f'{valor}', end=' ', flush=True)
+        sleep(0.3)
+        if contador == 0:
+            maior = valor
+        else:
+            if valor > maior:
+                maior = valor
+        contador += 1
+
+    print(f'Foram informados {contador} valores ao todo')
+    print(f'O maior valor informado foi {maior}')
+
+
+# Programa principal
+bigger(2, 9, 7, 83, 8, 14)
+bigger(7, 18, 9)
+bigger(1, 2)
+bigger()
