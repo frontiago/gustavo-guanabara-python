16730a1db6895d4a1007fdc5bb60ffdf2a2bca77
Thiago Silva <thiagowfer@gmail.com>
2023-12-19 08:17:12 -0300

    Finished exercise 100

--- exercicios/mundo3/aula20/ex100.py
+++ exercicios/mundo3/aula20/ex100.py
@@ -5,5 +5,29 @@
 # A primeira função vai sortear 5 números e vai colocá-los dentro da lista e a segunda função vai mostrar
 # a soma entre todos os valores PARES sorteados pela função anterior.
 
+from random import randint
+from time import sleep
+
+numeros = []
 
 
+def sorteia(lista):
+    print('Sorteando 5 valores da lista: ', end=' ')
+    for cont in range(0, 5):
+        n = randint(1, 100)
+        lista.append(n)
+        print(f'{n}', end=' ', flush=True)
+        sleep(0.3)
+
+
+def somapar(lista):
+    soma = 0
+    for valor in lista:
+        if valor % 2 == 0:
+            soma += valor
+    print(f'Somando os valores pares de {lista}, temos {soma}')
+
+
+sorteia(numeros)
+print('')
+somapar(numeros)
