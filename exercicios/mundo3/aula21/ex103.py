8beae297d1a2b45f15d0d6e1c21874f98df213f7
Thiago Silva <thiagowfer@gmail.com>
2023-12-26 08:01:55 -0300

    Finished exercise 103

--- exercicios/mundo3/aula21/ex103.py
+++ exercicios/mundo3/aula21/ex103.py
@@ -7,3 +7,20 @@
 # algum dado não teha sido informado corretamente.
 
 
+def ficha(jogador='Desconhecido', gols=0):
+    print(f'O jogador {jogador} fez {gols} gol(s) no campeonato')
+
+
+# Programa principal
+nome_jogador = str(input("Nome do jogador: "))
+gols_jogador = str(input("Número de gols: "))
+
+if gols_jogador.isnumeric():
+    gols_jogador = int(gols_jogador)
+else:
+    gols_jogador = 0
+
+if nome_jogador.strip() == '':
+    ficha(gols=gols_jogador)
+else:
+    ficha(nome_jogador, gols_jogador)
