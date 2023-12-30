57472d8072fe20a65b9a84063d56cad39e5f44c4
Thiago Silva <thiagowfer@gmail.com>
2023-12-30 08:51:05 -0300

    Finished exercise 105

--- exercicios/mundo3/aula21/ex105.py
+++ exercicios/mundo3/aula21/ex105.py
@@ -10,3 +10,32 @@
 # E) A situação (opcional)
 
 # Adicione também as docstrings da função
+
+def notas(* nota, situacao=False):
+    """
+    -> Função para analisar notas e situações de vários alunos
+    param n: uma ou mais notas dos alunos (aceita várias)
+    param situacao: valor opcional, indicando se deve ou não adicionar a situação
+    param return: dicionário com várias informações sobre a situação da turma
+    """
+    resultado = dict()
+    resultado["total"] = len(nota)
+    resultado["maior"] = max(nota)
+    resultado["menor"] = min(nota)
+    resultado["media"] = sum(nota)/len(nota)
+
+    if situacao:
+        if resultado["media"] >= 7:
+            resultado["situacao"] = "BOA"
+        elif resultado["media"] >= 5:
+            resultado["situacao"] = "RAZOAVEL"
+        elif resultado["media"] < 5:
+            resultado["situacao"] = "RUIM"
+
+    return resultado
+
+
+# Programa principal
+resposta = notas(5.5, 3.7, 4.9, 3.8, situacao=True)
+print(resposta)
+help(notas)
