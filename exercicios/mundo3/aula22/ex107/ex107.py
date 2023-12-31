4b4e5ac82da59de5d6af84387f81ac9cd0c505a5
Thiago Silva <thiagowfer@gmail.com>
2023-12-31 17:15:48 -0300

    Class 22 and questions from 107 to 112

---
+++ exercicios/mundo3/aula22/ex107/ex107.py
@@ -0,0 +1,6 @@
+# Exercício 107 -
+#
+
+# Crie um módulo chamado moeda.py que tenha funções incorporadas aumentar(), diminuir(), dobro(), metade()
+# Faça também um programa que importe esse módulo e use algumas dessas funções.
+
---
+++ exercicios/mundo3/aula22/ex110/ex110.py
@@ -0,0 +1,6 @@
+# Exercício 110 -
+#
+
+# Adicione ao módulo moedas.py criado nos desafios anteriores, uma função chamada resumo(), que mostre
+# na tela algumas informações geradas pelas funções que já temos no módulo criado até aqui
+
